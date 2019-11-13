bf9a103b812c98742bb3dc6a68b06ed882ab3245
Moritz Rogalski <moritz.rogalski@gmail.com>
2019-11-13 20:05:29 +0100

    Testdateien hinzugefügt

--- Test.py
+++ Test.py
@@ -3,17 +3,17 @@ import pdf2image
 # from pdf2image import pdf2image
 from BussgeldExtractor import Extractor
 
-pdf2image.convert_from_path("resources/ideal-images/600dpi/BußgeldVorlage1Scan2-600dpi.pdf", fmt='png', dpi='600', output_folder="resources/ideal-images/600dpi")
+#pdf2image.convert_from_path("resources/ideal-images/TestbilderWordSerienbriefe/BußgeldbescheidZwickauTest.pdf", fmt='png', dpi='600', output_folder="resources/ideal-images/TestbilderWordSerienbriefe")
 
 # extr = Extractor("resources/ideal-images/300dpi/d5debecb-5cb4-46da-890c-9ac60dcdb40c-1.png")
 # extr = Extractor("resources/ideal-images/600dpi/f8bc9163-a6d6-44d9-bd5d-0baac4dfe4c7-1.png")
 # extr = Extractor("resources/ideal-images/smartphone/7a8b2627-328f-45bd-b062-f51c1508d1d8-1.png")
-# extr = Extractor(r"ocr-bussgeld\resources\images\BB_K-D-U-V-B-O-AV_5.jpg")
+extr = Extractor(r"resources\ideal-images\TestbilderWordSerienbriefe\ZwickauTestNr1.png")
 # extr = Extractor("resources/images/BB_K-D-U-V-B-A-T-O-AV_15.jpg")
 # extr = Extractor("resources/images/BB_D-U-V-AV_1.JPG")
 # extr = Extractor(r"resources/ideal-images/600dpi/Bußgeldbescheid-Aalen.png")
 
 
-# context = extr.get_information_context()
-# print(json.dumps(context, default=lambda x: x.__dict__, ensure_ascii=False))
+context = extr.get_information_context()
+print(json.dumps(context, default=lambda x: x.__dict__, ensure_ascii=False))
 
