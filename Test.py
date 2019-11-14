e9e931c72d5a2f8513d2367e3d23734118790824
Moritz Rogalski <moritz.rogalski@gmail.com>
2019-11-14 10:50:18 +0100

    Testfälle erweitert

--- Test.py
+++ Test.py
@@ -3,15 +3,15 @@ import pdf2image
 # from pdf2image import pdf2image
 from BussgeldExtractor import Extractor
 
-#pdf2image.convert_from_path("resources/ideal-images/TestbilderWordSerienbriefe/BußgeldbescheidZwickauTest.pdf", fmt='png', dpi='600', output_folder="resources/ideal-images/TestbilderWordSerienbriefe")
+# pdf2image.convert_from_path("resources/ideal-images/TestbilderWordSerienbriefe/BußgeldbescheidKonstanzTest.pdf", fmt='png', dpi='600', output_folder="resources/ideal-images/TestbilderWordSerienbriefe")
 
 # extr = Extractor("resources/ideal-images/300dpi/d5debecb-5cb4-46da-890c-9ac60dcdb40c-1.png")
 # extr = Extractor("resources/ideal-images/600dpi/f8bc9163-a6d6-44d9-bd5d-0baac4dfe4c7-1.png")
 # extr = Extractor("resources/ideal-images/smartphone/7a8b2627-328f-45bd-b062-f51c1508d1d8-1.png")
-extr = Extractor(r"resources\ideal-images\TestbilderWordSerienbriefe\ZwickauTestNr1.png")
+# extr = Extractor(r"resources\ideal-images\TestbilderWordSerienbriefe\ZwickauTestNr1.png")
 # extr = Extractor("resources/images/BB_K-D-U-V-B-A-T-O-AV_15.jpg")
 # extr = Extractor("resources/images/BB_D-U-V-AV_1.JPG")
-# extr = Extractor(r"resources/ideal-images/600dpi/Bußgeldbescheid-Aalen.png")
+extr = Extractor(r"resources\ideal-images\TestbilderWordSerienbriefe\ZwickauTestNr1.png")
 
 
 context = extr.get_information_context()
